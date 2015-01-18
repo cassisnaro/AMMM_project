774713d4678165d0cebeb616b9d7815b2d46b9de
cassisnaro <narowirf@gmail.com>
2015-01-18 22:32:29 +0100

    10 iterations of GRASP, state of RCLs maintained

--- src/dctransfers/Main.java
+++ src/dctransfers/Main.java
@@ -9,7 +9,9 @@ import java.net.URISyntaxException;
 public class Main {
 
 	public static void main(String... aArgs) throws IOException, URISyntaxException {
+		long startILPconversion= System.currentTimeMillis();
 		ReadWithScanner parser = new ReadWithScanner();
 		WriteWithPrintWriter writer = new WriteWithPrintWriter(parser.getGraph(), parser.getTransferCollections(), parser.getRequestedTransfer());
+		System.out.println("Conversion time: "+(System.currentTimeMillis()-startILPconversion));
 	}
 }
